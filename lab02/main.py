5e36b13324de38ab4a40ccd51a127061a7d4da1d
Den <denis100703@gmail.com>
2023-10-31 06:50:43 +0300

    Folder structure changed

--- lab02/main.py
+++ lab02/main.py
@@ -1,19 +1,17 @@
 from cipher import *
 
-aes = AES(b'0123456789abcdef')
 key = b'0123456789abcdef'
 
 input_filename = 'assets/mars.bmp'
 
-ecb_ecrypted_filename = 'assets/ecb_enc.bmp'
-ecb_decrypted_filename = 'assets/ecb_dec.bmp'
+ecb_enc_filename = 'encrypted/ecb_enc.bmp'
+ecb_dec_filename = 'encrypted/ecb_dec.bmp'
 
-cbc_encrypted_filename = 'assets/cbc_enc.bmp'
-cbc_decrypted_filename = 'assets/cbc_dec.bmp'
+cbc_enc_filename = 'encrypted/cbc_enc.bmp'
+cbc_dec_filename = 'encrypted/cbc_dec.bmp'
 
+encrypt_bmp(key, input_filename, ecb_enc_filename, mode=BlockMode.ECB)
+decrypt_bmp(key, ecb_enc_filename, ecb_dec_filename, mode=BlockMode.ECB)
 
-encrypt_bmp(key, input_filename, ecb_ecrypted_filename, mode=BlockMode.ECB)
-decrypt_bmp(key, ecb_ecrypted_filename, ecb_decrypted_filename, mode=BlockMode.ECB)
-
-encrypt_bmp(key, input_filename, cbc_encrypted_filename, mode=BlockMode.CBC)
-decrypt_bmp(key, cbc_encrypted_filename, cbc_decrypted_filename, mode=BlockMode.CBC)
+encrypt_bmp(key, input_filename, cbc_enc_filename, mode=BlockMode.CBC)
+decrypt_bmp(key, cbc_enc_filename, cbc_dec_filename, mode=BlockMode.CBC)
